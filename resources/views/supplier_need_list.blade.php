d68549d1e97de9d84e357912c30e90e1f5cd30e2
逯力源 <953909391@qq.com>
2018-02-05 14:46:24 +0800

    no message

--- resources/views/supplier_need_list.blade.php
+++ resources/views/supplier_need_list.blade.php
@@ -66,7 +66,7 @@
 							  	<a class="pvr-caozuo" style="" onclick="supplierView(this,{{$item->offer_id}})">查看订单</a>
 							  	  @elseif($item['status'] == '2')
 							  	  <a class="pvr-caozuo" onclick="supplierView(this,{{$item->offer_id}})">查看订单</a>
-							  	  	<a class="pvr-caozuo" style="color: #333;"onclick="SendGoods(this,{{$item->offer_id}})" >准备配货</a>
+							  	  	<a class="pvr-caozuo" style="color: #4284c5;"onclick="SendGoods(this,{{$item->offer_id}})" >准备配货</a>
 							  	  	  @elseif($item['status'] == '3')
 							  	  	  <a class="pvr-caozuo" onclick="supplierView(this,{{$item->offer_id}})">查看订单</a>
 							  	  	  <a class="pvr-caozuo" >已发货</a>
