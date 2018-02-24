796f3f9d70f357c8e86d6947781417c9d0c2eccf
逯力源 <953909391@qq.com>
2018-02-24 14:23:58 +0800

    关闭按钮

--- resources/views/cart_list.blade.php
+++ resources/views/cart_list.blade.php
@@ -203,7 +203,7 @@
 		      maxmin: false,
 		      fixed :false,
 		      shadeClose: true, //点击遮罩关闭层
-		      area : ['965px' , '800px'],
+		      area : ['965px' , '80%'],
 		      content: '{{url('army/need/view/release')}}'+"/"+xiadanData2
 		    });
 			}
